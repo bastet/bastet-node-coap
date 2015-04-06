685203209c59fe78c05eca0ff818c2380d516222
Dan Atkinson <hourd.tasa@gmail.com>
2015-04-06 17:57:32 +0100

    Added a new example route with multiple slugs

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
-const coap  = require('coap');
+const coap = require('coap');
 var host = 'localhost';
 
-var names = ["Dan", "Martin"];
+var names = ["Dan", "Martin","slug/snail/worm"];
 names.forEach(function(entry) {
 	var request = coap.request('coap://'+host+'/'+entry);
 	request.on('response', function(res) {

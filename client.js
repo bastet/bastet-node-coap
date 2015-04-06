6f4a0eb5e31872392f8a52130bfcb2bc2fc0d4ec
Dan Atkinson <hourd.tasa@gmail.com>
2015-04-06 18:34:04 +0100

    Added some more default test routes

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 const coap = require('coap');
 var host = 'localhost';
 
-var names = ["Dan", "Martin","slug/snail/worm"];
+var names = ["Dan", "Martin","slug/snail/worm", "hostname", "ip"];
 names.forEach(function(entry) {
 	var request = coap.request('coap://'+host+'/'+entry);
 	request.on('response', function(res) {

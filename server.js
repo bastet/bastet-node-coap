c555dd24c73048f1a4a4c3b65f30d426b445acb2
Dan Atkinson <hourd.tasa@gmail.com>
2015-04-06 18:35:10 +0100

    Added some default node endroutes and all responses are now JSON

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const coap = require('coap');
 var server = coap.createServer();
 var application = {};
 
+var os = require('os');
+var ifaces = os.networkInterfaces();
+
 var router = {
     'routes': [{'url': 'example', 'function': 'exampleFunction'}],
     'getRoutes': function() { return this.routes; },
@@ -10,6 +13,7 @@ var router = {
     },
     // Custom basic router
     'parseRoute': function(routeString) {
+        var result = "null";
         var initialComponents = routeString.split('/');
         var urlComponents = [];
         initialComponents.forEach(function(item) {
@@ -19,29 +23,61 @@ var router = {
         this.routes.forEach(function(route) {
             // if a match is found then try and run the assigned function
             if(route.url === urlComponents[0]) {
+                console.log('Valid requested route: '+route.url);
                 if(route.function in application && typeof application[route.function] === 'function') {
-                    application[route.function](urlComponents);
+                    result = application[route.function](urlComponents);
                 }
             }
         });
+        return result;
     }
 };
 
 // Define new routes
 router.addRoute('slug', 'printSlug');
+router.addRoute('hostname', 'getHostname');
+router.addRoute('ip', 'getIP');
 
 // Define custom functions
-application.exampleFunction = function exampleFunction() { console.log('This is an example function.'); };
+application.exampleFunction = function exampleFunction() { retun('This is an example function.'); };
+
+application.getHostname = function getHostname() {
+    return(os.hostname());
+};
+
+application.getIP = function getIP() {
+    var IP = "no ip";
+    Object.keys(ifaces).forEach(function (ifname) {
+    var alias = 0;
+    ifaces[ifname].forEach(function (iface) {
+      if ('IPv4' !== iface.family || iface.internal !== false) {
+        // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+        return;
+      }
+      if (alias >= 1) {
+        // this single interface has multiple ipv4 addresses
+        IP = (ifname + ':' + alias, iface.address);
+      } else {
+        // this interface has only one ipv4 adress
+        IP = iface.address;
+      }
+    });
+  });
+  return IP;
+};
 
 application.printSlug = function printSlug(urlComponents) {
-    console.log('printSlugs');
-    console.log(urlComponents);
+    return(urlComponents);
 };
 
 // event on recieving a request
 server.on('request', function(req, res) {
-  router.parseRoute(req.url);
-  res.end('Hello ' + req.url.split('/')[1] + '\n')
+  var result = router.parseRoute(req.url);
+  if(result === 'null' || typeof result === 'undefined') {
+    res.end(JSON.stringify('Hello ' + req.url.split('/')[1]) + '\n');
+  } else {
+    res.end(JSON.stringify(result)+'\n');
+  }
 })
 
 // start the server listening

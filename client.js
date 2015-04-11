407867400aae6ebcb61fda0710ea5dfcebe7bbe5
Dan Atkinson <hourd.tasa@gmail.com>
2015-04-11 09:42:08 +0100

    Added new test routes and some console logs to make the terminal output a bit easier to read for testing

--- client.js
+++ client.js
@@ -1,11 +1,13 @@
 const coap = require('coap');
 var host = 'localhost';
 
-var names = ["Dan", "Martin","slug/snail/worm", "hostname", "ip"];
+var names = ["Dan", "Martin","slug/snail/worm", "hostname", "ip", "cpu", "memory", "type", "platform", "arch", "release", "uptime", "load", "network"];
 names.forEach(function(entry) {
 	var request = coap.request('coap://'+host+'/'+entry);
 	request.on('response', function(res) {
-		res.pipe(process.stdout)
+        console.log('');
+        console.log('Request: '+entry);
+		res.pipe(process.stdout);
 	})
 	request.end()
 });
